cf0e4bb7b061ccbb09b34c03cbc055bb4703d4f8
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-03-27 17:31:32 -0500

    3-27-23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
